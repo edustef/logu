8d03e41cc700556c72f636ea8f11bea8f040cb25
Eduard Stefan <eduardstefan290@gmail.com>
2021-06-03 19:55:14 +0200

    added simple cross button

---
+++ components/XButton.tsx
@@ -0,0 +1,10 @@
+import { XIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+export default function XButton() {
+	return (
+		<>
+			<XIcon className='w-5 h-5 m-1' />
+		</>
+	)
+}
