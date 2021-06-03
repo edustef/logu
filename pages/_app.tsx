1953425545dbfe69fa1fdbd23143aecad3585a27
Eduard Stefan <eduardstefan290@gmail.com>
2021-06-03 19:55:50 +0200

    added toast notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,11 +10,25 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { SkeletonTheme } from 'react-loading-skeleton'
 import { setLocale } from 'yup'
 import GoogleOneTapProvider from '../hooks/useGoogleOneTap'
+import 'react-toastify/dist/ReactToastify.css'
 import Head from 'next/head'
+import { ToastContainer } from 'react-toastify'
+import { XIcon } from '@heroicons/react/outline'
+import XButton from '../components/XButton'
+import clsx from 'clsx'
 
 const queryClient = new QueryClient()
 
 const App = ({ Component, pageProps }: AppProps) => {
+	const contextClass = {
+		success: 'bg-blue-600',
+		error: 'bg-red-600',
+		info: 'bg-gray-600',
+		warning: 'bg-red-400',
+		default: 'bg-indigo-600',
+		dark: 'bg-gray-darkless'
+	}
+
 	// configure dayjs and plugins
 	dayjs.extend(localizedFormat)
 	dayjs.extend(calendarPlugin)
@@ -48,6 +62,19 @@ const App = ({ Component, pageProps }: AppProps) => {
 					<GoogleOneTapProvider>
 						<LanguageProvider>
 							<SkeletonTheme color='#374151' highlightColor='#4b5563'>
+								<ToastContainer
+									toastClassName={({ type }) =>
+										clsx(
+											contextClass[type || 'default'],
+											'relative text-white flex p-1 min-h-10 shadow-md justify-between overflow-hidden cursor-pointer'
+										)
+									}
+									bodyClassName={() => 'text-sm font-white font-med block p-3'}
+									position='top-right'
+									autoClose={6000}
+									draggable={false}
+									closeButton={XButton}
+								/>
 								<Component {...pageProps} />
 							</SkeletonTheme>
 						</LanguageProvider>
