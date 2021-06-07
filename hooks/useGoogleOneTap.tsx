aad90f3ddc5aebcf7b15633753647dfd08be3767
Eduard Stefan <eduardstefan290@gmail.com>
2021-06-07 17:16:59 +0200

    moved context functionality to context folder

--- hooks/useGoogleOneTap.tsx
+++ hooks/useGoogleOneTap.tsx
@@ -1,51 +1,6 @@
-import { useState, useEffect, createContext, useContext } from 'react'
-import { signIn } from 'next-auth/client'
+import { useContext, useEffect } from 'react'
+import { GoogleOneTapContext } from '../context/googleOneTapProvider'
 
-declare const google: any
-
-interface Props {
-	children: React.ReactNode
-}
-
-export const GoogleOneTapContext = createContext<[string, React.Dispatch<string>]>(undefined)
-
-const GoogleOneTapProvider: React.FC<Props> = ({ children }) => {
-	const [idToken, setIdToken] = useState(null)
-	const [clientId, setClientId] = useState(null)
-
-	// DISPLAY GOOGLE ONE TAP UI
-	useEffect(() => {
-		if (clientId) {
-			const handleCredentialResponse = (response) => {
-				setIdToken(response.credential)
-			}
-
-			google.accounts.id.initialize({
-				client_id: clientId,
-				callback: handleCredentialResponse,
-				context: 'use',
-				prompt_parent_id: 'google-one-tap',
-				cancel_on_tap_outside: false
-			})
-
-			setTimeout(() => {
-				google.accounts.id.prompt()
-			}, 2000)
-		}
-	}, [clientId])
-
-	// WHEN idToken IS SET, SIGN IN THE USER
-	useEffect(() => {
-		if (idToken) {
-			// Sign in with credential from the Google user.
-			signIn('google')
-		}
-	}, [idToken])
-
-	return <GoogleOneTapContext.Provider value={[clientId, setClientId]}>{children}</GoogleOneTapContext.Provider>
-}
-
-export default GoogleOneTapProvider
 
 export const useGoogleOneTap = () => {
 	const googleClientId = '187630268462-4qotigsap0ghe6hr5c9rj8ii6tg2dekj.apps.googleusercontent.com'
--- hooks/useLanguage.tsx
+++ hooks/useLanguage.tsx
@@ -1,43 +1,6 @@
-import dayjs from 'dayjs'
-import { useRouter } from 'next/router'
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { Language, LanguageContext } from '../context/languageProvider'
 
-interface Props {
-	children: React.ReactNode
-}
-
-export interface Language {
-	name: string
-	locale: string
-}
-
-export const languages: Language[] = [
-	{
-		name: 'English',
-		locale: 'en'
-	},
-	{
-		name: 'Español',
-		locale: 'es'
-	}
-]
-
-export const LanguageContext = React.createContext<[Language, React.Dispatch<Language>]>(undefined)
-
-const LanguageProvider: React.FC<Props> = ({ children }) => {
-	const router = useRouter()
-	const [currentLanguage, setCurrentLanguage] = useState(
-		languages.find((language) => language.locale === router.locale)
-	)
-
-	useEffect(() => {
-		dayjs.locale(currentLanguage.locale)
-	}, [currentLanguage])
-
-	return <LanguageContext.Provider value={[currentLanguage, setCurrentLanguage]}>{children}</LanguageContext.Provider>
-}
-
-export default LanguageProvider
 
 export const useLanguage = (): [Language, React.Dispatch<Language>] => {
 	const [currentLanguage, setCurrentLanguage] = useContext(LanguageContext)
