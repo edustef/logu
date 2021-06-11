cc16588e3cfd9f1d6941fc5bef73b8df79d4e443
Eduard Stefan <eduardstefan290@gmail.com>
2021-06-12 00:35:20 +0200

    improved some typings

--- hooks/useWorkspaces.ts
+++ hooks/useWorkspaces.ts
@@ -2,10 +2,11 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 import { Workspace } from '.prisma/client'
 import { WorkspaceParams } from '../models/WorkspaceParams'
+import { WorkspaceWithUsers } from '../schemas/userWorkspace.schema'
 
 export default function useWorkspaces(props: WorkspaceParams = {}) {
 	return useQuery(['workspaces', props], async () => {
-		const { data } = await axios.get<Workspace[]>('/api/workspaces', {
+		const { data } = await axios.get<WorkspaceWithUsers[]>('/api/workspaces', {
 			params: props
 		})
 
